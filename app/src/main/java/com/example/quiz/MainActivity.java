5ea8f9e2616c8747b85beba857aff0506c766122
Ilya <kochev0987@gmail.com>
2023-05-27 21:26:30 +0300

    me

---
+++ app/src/main/java/com/example/quiz/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.quiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+TextView totalQuestionsTextView;
+int score=0;
+int totalQuestions=QuestionAnswer.question.length;
+TextView questionTextView;
+int currentQuestionIndex=0;
+String selectedAnswer = "";
+Button ansA;
+Button ansB;
+Button ansC;
+Button ansD;
+Button submitBtn;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        totalQuestionsTextView=findViewById(R.id.TotalQuestions);
+        questionTextView=findViewById(R.id.question);
+        ansA=findViewById(R.id.ans_1);
+        ansB=findViewById(R.id.ans_2);
+        ansC=findViewById(R.id.ans_3);
+        ansD=findViewById(R.id.ans_4);
+        submitBtn=findViewById(R.id.submit);
+        totalQuestionsTextView.setText("Total Questions : "+totalQuestions);
+
+        loadNewQuestion();
+    }
+
+    @Override
+    public void onClick(View v) {
+        ansA.setOnClickListener(this);
+        ansB.setOnClickListener(this);
+        ansC.setOnClickListener(this);
+        ansD.setOnClickListener(this);
+        submitBtn.setOnClickListener(this);
+
+        Button clickedButton=(Button) v;
+
+        if(clickedButton.getId()==R.id.submit){
+            if(selectedAnswer.equals(QuestionAnswer.correct_answers[currentQuestionIndex])) {
+                score++;
+            }
+            currentQuestionIndex++;
+            loadNewQuestion();
+
+
+        }
+        else{
+            selectedAnswer=clickedButton.getText().toString();
+            ansA.setBackgroundColor(Color.WHITE);
+            ansB.setBackgroundColor(Color.WHITE);
+            ansC.setBackgroundColor(Color.WHITE);
+            ansD.setBackgroundColor(Color.WHITE);
+            clickedButton.setBackgroundColor(Color.BLUE);
+        }
+    }
+    void loadNewQuestion(){
+        questionTextView.setText(QuestionAnswer.question[currentQuestionIndex]);
+        ansA.setText(QuestionAnswer.choices[currentQuestionIndex][0]);
+        ansB.setText(QuestionAnswer.choices[currentQuestionIndex][1]);
+        ansC.setText(QuestionAnswer.choices[currentQuestionIndex][2]);
+        ansD.setText(QuestionAnswer.choices[currentQuestionIndex][3]);
+        if (currentQuestionIndex==totalQuestions){
+        FinishQuiz();
+        return;
+        }
+    }
+    void FinishQuiz(){
+        String results="";
+        if(score>totalQuestions*0.60){
+            results="You did well";
+        }
+        else
+        results ="You can always do better";
+        new AlertDialog.Builder(this).setTitle(results).setMessage("Your score is:"+ score+"out of"+ totalQuestions)
+                .setPositiveButton("Restart",(dialogueInterface,i)->restartQuiz());
+    }
+    void restartQuiz()
+    {
+        score=0;
+        currentQuestionIndex=0;
+        loadNewQuestion();
+    }
+
+}
---
+++ app/src/main/java/com/example/quiz/QuestionAnswer.java
@@ -0,0 +1,208 @@
+package com.example.quiz;
+
+public class QuestionAnswer {
+    public static String question[]={"Which country is the world’s largest producer of cheese?",
+
+            "What goes into a White Russian cocktail?",
+        "Which film featured ‘King of Wishful Thinking’ by Go West?",
+
+                "Who was Henry VIII’s 3rd wife?",
+
+        "Which artist painted the Poppy Field in 1873?",
+
+               "Which Beatles film featured the songs ‘You’re Going to Lose that Girl’ and Beethoven’s Symphony Number 9 in D Minor?",
+
+                "What does GIF stand for?",
+
+        "In which fictional town is Coronation Street set in?",
+
+                "Which Spice Girl has appeared as an extra in Emmerdale?",
+
+                "In the Wizard of Oz, what was the lion searching for?",
+
+        "Which band recorded the 1977 album Rumours?",
+
+        "In which area of London did the \"Jack the Ripper\" murders take place in 1888?",
+
+                "In which African country would you find the Skeleton Coast?",
+
+                "In which conflict did Florence Nightingale develop her nursing techniques?",
+
+                "In which year did the first McDonald's restaurant open?",
+
+        "At what age did rock stars Kurt Cobain, Brian Jones and Jim Morrison all die?",
+
+        "What is the capital of Canada? 25",
+
+        "Which country defeated Britain in the battle for Singapore?",
+
+                "Which country has the most colours on its flag?",
+
+        "In the human body what connects the bladder to the kidneys?"};
+    public static String choices[][]
+        ={{
+            "USA",
+            "Pretty Woman",
+
+            "Jane Seymour",
+
+            "Claude Monet",},{
+            "Vodka, coffee liqueur, cream/milk",
+            "Pretty Woman",
+
+            "Jane Seymour",},{
+
+            "Claude Monet",
+            "Pretty Woman",
+            "USA",
+
+            "Jane Seymour",},{
+
+            "Claude Monet",
+
+            "Pretty Woman",
+            "USA",
+
+            "Jane Seymour",},{
+
+            "Claude Monet",
+
+            "Pretty Woman",
+            "USA",
+
+            "Jane Seymour",},{
+
+            "Claude Monet",
+
+
+            "Help!",
+            "USA",
+
+            "Jane Seymour",},{
+
+            "Claude Monet",
+            "Graphic Interchange Format",
+            "USA",
+
+            "Jane Seymour",},{
+
+            "Claude Monet",
+            "Weatherfield",
+            "USA",
+
+            "Jane Seymour",},{
+
+            "Claude Monet",
+            "Mel B",
+            "USA",
+
+            "Jane Seymour",},{
+
+            "Claude Monet",
+            "Courage",
+            "USA",
+
+            "Jane Seymour",},{
+
+            "Claude Monet",
+            "Fleetwood Mac",
+            "USA",
+
+            "Jane Seymour",},{
+
+            "Claude Monet",
+            "Whitechapel",
+            "USA",
+
+            "Jane Seymour",},{
+
+            "Claude Monet",
+            "Namibia",
+            "USA",
+
+            "Jane Seymour",},{
+
+            "Claude Monet",
+            "Crimean War",
+            "USA",
+
+            "Jane Seymour",},{
+
+            "Claude Monet",
+            "1940",
+            "USA",
+
+            "Jane Seymour",},{
+
+            "Claude Monet",
+            "27",
+            "USA",
+
+            "Jane Seymour",},{
+
+            "Claude Monet",
+            "Ottawa",
+            "USA",
+
+            "Jane Seymour",},{
+
+            "Claude Monet",
+            "Japan",
+            "USA",
+
+            "Jane Seymour",},{
+
+            "Claude Monet",
+            "Belize",
+            "USA",
+
+            "Jane Seymour",},{
+
+            "Claude Monet",
+            "Ureters",
+            "USA",
+
+            "Jane Seymour",}
+    };
+    public static String correct_answers[]={"USA",
+
+            "Vodka, coffee liqueur, cream/milk",
+
+            "Pretty Woman",
+
+            "Jane Seymour",
+
+            "Claude Monet",
+
+            "Help!",
+
+            "Graphic Interchange Format",
+
+            "Weatherfield",
+
+            "Mel B",
+
+            "Courage",
+
+            "Fleetwood Mac",
+
+            "Whitechapel",
+
+            "Namibia",
+
+            "Crimean War",
+
+            "1940",
+
+            "27",
+
+            "Ottawa",
+
+            "Japan",
+
+            "Belize",
+
+            "Ureters",};
+
+
+}
