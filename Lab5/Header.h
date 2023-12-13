c690db683d47b1b78925bea24eac75d5abc3da36
Ilya <kochev0987@gmail.com>
2023-12-13 18:31:34 +0300

    Have a thing

--- Lab5/Header.h
+++ Lab5/Header.h
@@ -3,33 +3,26 @@
 
 char lpszInMessage[80];
 int readingMessage(HANDLE hNamedPipe, char* inMessage);
-int requestProcessing(char* inMessage, int& index);
+int requestProcessing(char* inMessage);
 int writingMessage(HANDLE hNamedPipe);
 int waitingClient(HANDLE hNamedPipe);
-int readRequest(int&);
-int modifyRequest(int&, HANDLE);
+int readRequest();
+int modifyRequest(HANDLE);
 using namespace std;
 
 
 extern char c; // служебный символ
-extern SECURITY_ATTRIBUTES sa; // атрибуты защиты
-extern SECURITY_DESCRIPTOR sd; // дескриптор защиты
 extern STARTUPINFO startupInfo;
 extern DWORD dwBytesRead; // для числа прочитанных байтов
 extern char OutMessage[80]; // обратное сообщение
-extern DWORD dwBytesWrite; // для числа записанных байтов
-extern int processAmount;
-extern PROCESS_INFORMATION processInfo;
-extern HANDLE* processHandles;
-extern const wchar_t* pipeNameBase;
 extern HANDLE* hNamedPipes;
 extern char InMessage[80];
+extern int index;
 
 
 
 
 char inMessage[10];
-int index;
 int processingThread(int i)
 {
 	if (waitingClient(hNamedPipes[i]) == 1)
@@ -40,11 +33,11 @@ int processingThread(int i)
 		if (readingMessage(hNamedPipes[i], inMessage) == 1)
 			return 0;
 
-		int desicion = requestProcessing(inMessage, index);
+		int desicion = requestProcessing(inMessage);
 		std::cout << "desicion: " << desicion << "\n";
 		if (desicion == 2)
 		{
-			if (readRequest(index) == 1)
+			if (readRequest() == 1)
 				return 0;
 			// выводим полученное от клиента сообщение на консоль
 			std::cout << "The server has receivrd the following message from a client: "
@@ -60,7 +53,7 @@ int processingThread(int i)
 		else if (desicion == 0)
 		{
 			std::cout << "In case, when desicion = 0\n";
-			if (modifyRequest(index, hNamedPipes[i]) == 1)
+			if (modifyRequest(hNamedPipes[i]) == 1)
 				return 0;
 			std::cout << "After Modifying\n";
 		}
